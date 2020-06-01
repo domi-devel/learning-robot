0b7d8a6cabfc0bebc1abba5d061080b3242325bf
domi-devel <dominik.leherbauer@gmail.com>
2020-06-01 02:27:07 +0200

    fix interface test

--- tests/test_interfaces.py
+++ tests/test_interfaces.py
@@ -1,7 +1,10 @@
 import pytest
 from robotinterfaces import hello_world
+
 # from robotinterfaces import hello_world
+
 
 def test_hello_world():
     response = hello_world()
-    assert 'Hello world' == response
+    assert "Hello world 1" == response
+
