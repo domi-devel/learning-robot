9af91d405a671cccbf99d48f464c514358cd9ea8
domi-devel <dominik.leherbauer@gmail.com>
2020-07-08 11:59:52 +0200

    add UI implementation

--- robotcontrol/app/main/views.py
+++ robotcontrol/app/main/views.py
@@ -5,8 +5,17 @@ main = Blueprint("main", __name__, template_folder="templates/main", url_prefix=
 
 @main.route("/", methods=["GET"])
 def index():
-
     return render_template("main/index.html")
+
+
+@main.route("/load", methods=["GET"])
+def load():
+    return render_template("main/load.html")
+
+
+@main.route("/save", methods=["GET"])
+def save():
+    return render_template("main/save.html")
 
 
 @main.route("/post", methods=["POST"])
@@ -14,11 +23,26 @@ def handlepost():
     try:
         cmd = request.data.decode()
         print(cmd)
-        if(cmd == 'start'):
+        if "load" in cmd:
+            slot = cmd[-1:]
+            # do load slot
             return "OK"
-        elif(cmd == 'stop'):
+        elif "save" in cmd:
+            slot = cmd[-1:]
+            # do save
+            return "OK"
+        elif(cmd == 'idle'):
+            return "OK"
+        elif(cmd == 'work'):
+            return "OK"
+        elif(cmd == 'train'):
+            return "OK"
+        elif(cmd == 'shutdown'):
             return "OK"
         else:
             return('NOK')
     except Exception:
         return "Command Error"
+
+
+
--- robotcontrol/app/static/js/buttonhandler.js
+++ robotcontrol/app/static/js/buttonhandler.js
@@ -1,7 +1,5 @@
-
-
 function submitBtn(button) {
-    var currentLocation = window.location;
+    var currentLocation = window.location.origin;
     var xhttp = new XMLHttpRequest();
     // xhttp.onreadystatechange = function() {
     //   if (this.readyState == 4 && this.status == 200) {
@@ -9,6 +7,6 @@ function submitBtn(button) {
     //     this.responseText;
     //   }
     // };
-    xhttp.open("POST", currentLocation + "post", true);
+    xhttp.open("POST", currentLocation + "/post", true);
     xhttp.send(button);
   }
